50c26e78a6c5fa04e767027817967b7d3e69867b
niasua <nsuar@DESKTOP-G227FH7>
2025-07-14 16:17:29 -0300

    CRUD works correctly

--- HabitLogger/HabitLogger/obj/Release/net8.0/HabitLogger.AssemblyInfo.cs
+++ HabitLogger/HabitLogger/obj/Release/net8.0/HabitLogger.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitLogger")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b20cf76facf4b73cb79416a6457e1d4d051a250")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitLogger")]
